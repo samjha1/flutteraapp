887363d6358d87893a74b5ed817e07f35872f834
Shivam jha <106658479+samjha1@users.noreply.github.com>
2024-10-13 19:57:10 +0530

    Update login_page.dart
    login with backend code

--- login_page.dart
+++ login_page.dart
@@ -1,6 +1,24 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart'; // Import Firebase Auth
-import 'dashboard_page.dart'; // Import the dashboard page
+import 'package:http/http.dart' as http;
+import 'dashboard_page.dart'; // Ensure this file exists
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Login App',
+      home: LoginPage(),
+    );
+  }
+}
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -10,191 +28,78 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  bool _isLoading = false;
 
-  bool _obscurePassword = true;
-  bool _loginError = false;
-  String _errorMessage = '';
-  bool _isLoading = false; // Loading state
-
-  final FirebaseAuth _auth = FirebaseAuth.instance; // Firebase Auth instance
-
-  Future<void> _login() async {
+  Future<void> _signIn() async {
     setState(() {
-      _isLoading = true; // Start loading
+      _isLoading = true;
     });
 
-    try {
-      await _auth.signInWithEmailAndPassword(
-        email: _emailController.text,
-        password: _passwordController.text,
+    // The API URL of your XAMPP server (make sure XAMPP Apache server is running)
+    const String apiUrl = 'http://localhost/api/login.php';
+
+    // Send a POST request to the PHP server
+    final response = await http.post(
+      Uri.parse(apiUrl),
+      body: {
+        'username': _usernameController.text,
+        'password': _passwordController.text,
+      },
+    );
+
+    // Decode the response
+    final Map<String, dynamic> responseData = json.decode(response.body);
+
+    if (responseData['success'] == "true") {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(responseData['message'])),
       );
-      // Navigate to the Dashboard if login is successful
+
+      // Navigate to DashboardPage after successful login
       Navigator.pushReplacement(
         context,
-        MaterialPageRoute(builder: (context) => const DashboardScreen()),
+        MaterialPageRoute(builder: (context) => const DashboardPage()),
       );
-    } on FirebaseAuthException catch (e) {
-      setState(() {
-        _loginError = true;
-        _errorMessage = e.message ?? 'Login failed';
-      });
-    } finally {
-      setState(() {
-        _isLoading = false; // Stop loading
-      });
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(responseData['message'])),
+      );
     }
-  }
 
-  @override
-  void dispose() {
-    _emailController.dispose();
-    _passwordController.dispose();
-    super.dispose();
+    setState(() {
+      _isLoading = false;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Colors.blue.shade900, Colors.blue.shade600],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-          ),
-        ),
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  'Login',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 32.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(height: 40.0),
-                if (_loginError)
-                  Text(
-                    _errorMessage,
-                    style: const TextStyle(color: Colors.red),
-                  ),
-                const SizedBox(height: 10.0),
-                TextField(
-                  controller: _emailController,
-                  decoration: InputDecoration(
-                    prefixIcon: const Icon(Icons.email, color: Colors.white),
-                    labelText: 'Email',
-                    labelStyle: const TextStyle(color: Colors.white),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(color: Colors.white),
-                      borderRadius: BorderRadius.circular(12.0),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.blue.shade300),
-                      borderRadius: BorderRadius.circular(12.0),
-                    ),
-                  ),
-                  style: const TextStyle(color: Colors.white),
-                  onChanged: (text) {
-                    if (_loginError) {
-                      setState(() {
-                        _loginError = false;
-                        _errorMessage = '';
-                      });
-                    }
-                  },
-                ),
-                const SizedBox(height: 20.0),
-                TextField(
-                  controller: _passwordController,
-                  obscureText: _obscurePassword,
-                  decoration: InputDecoration(
-                    prefixIcon: const Icon(Icons.lock, color: Colors.white),
-                    labelText: 'Password',
-                    labelStyle: const TextStyle(color: Colors.white),
-                    suffixIcon: IconButton(
-                      icon: Icon(
-                        _obscurePassword
-                            ? Icons.visibility_off
-                            : Icons.visibility,
-                        color: Colors.white,
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          _obscurePassword = !_obscurePassword;
-                        });
-                      },
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(color: Colors.white),
-                      borderRadius: BorderRadius.circular(12.0),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.blue.shade300),
-                      borderRadius: BorderRadius.circular(12.0),
-                    ),
-                  ),
-                  style: const TextStyle(color: Colors.white),
-                  onChanged: (text) {
-                    if (_loginError) {
-                      setState(() {
-                        _loginError = false;
-                        _errorMessage = '';
-                      });
-                    }
-                  },
-                ),
-                const SizedBox(height: 40.0),
-                ElevatedButton(
-                  onPressed: _isLoading ? null : _login,
-                  style: ElevatedButton.styleFrom(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 40.0, vertical: 15.0),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12.0),
-                    ),
-                    backgroundColor: Colors.blue.shade300,
-                  ),
-                  child: _isLoading
-                      ? const CircularProgressIndicator(
-                          valueColor:
-                              AlwaysStoppedAnimation<Color>(Colors.white),
-                        )
-                      : const Text(
-                          'Login',
-                          style: TextStyle(fontSize: 18.0, color: Colors.white),
-                        ),
-                ),
-                const SizedBox(height: 20.0),
-                TextButton(
-                  onPressed: () {
-                    // Implement password reset functionality
-                    // You can use Firebase's sendPasswordResetEmail method here
-                  },
-                  child: const Text(
-                    'Forgot Password?',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-                TextButton(
-                  onPressed: () {
-                    // Navigate to sign-up page
-                  },
-                  child: const Text(
-                    'Don’t have an account? Sign Up',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-              ],
+      appBar: AppBar(
+        title: const Text('Login Page'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _usernameController,
+              decoration: const InputDecoration(labelText: 'Username'),
             ),
-          ),
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(labelText: 'Password'),
+              obscureText: true, // To hide the password as the user types
+            ),
+            const SizedBox(height: 20),
+            _isLoading
+                ? const CircularProgressIndicator()
+                : ElevatedButton(
+                    onPressed: _signIn,
+                    child: const Text('Login'),
+                  ),
+          ],
         ),
       ),
     );
