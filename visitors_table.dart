6d047f135011f9fcd0e31983998c00decf91ceb1
Shivam jha <106658479+samjha1@users.noreply.github.com>
2024-12-03 17:02:49 +0530

    Update visitors_table.dart

--- visitors_table.dart
+++ visitors_table.dart
@@ -1,8 +1,10 @@
-import 'package:data_table_2/data_table_2.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'SettingsScreen.dart';
+import 'front_page.dart';
 import 'visitors.dart';
+import 'profile.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,9 +16,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Visitors Table',
+      title: 'Visitors List',
       theme: ThemeData.dark().copyWith(
-        primaryColor: const Color.fromARGB(255, 23, 146, 207),
+        primaryColor: const Color(0xFF800000),
         scaffoldBackgroundColor: const Color.fromARGB(255, 9, 59, 133),
         textTheme: const TextTheme(
           bodyLarge: TextStyle(color: Colors.white),
@@ -53,7 +55,7 @@ class _VisitorsdataState extends State<Visitorsdata> {
     });
 
     // Construct the URL with the optional search query
-    String url = 'https://api.indataai.in/durga/get_all_visitors.php';
+    String url = 'https://api.indataai.in/durga/visitorsdetails.php';
     if (searchQuery != null && searchQuery.isNotEmpty) {
       url += '?search=$searchQuery';
     }
@@ -74,8 +76,14 @@ class _VisitorsdataState extends State<Visitorsdata> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Visitors Table'),
-        backgroundColor: const Color.fromARGB(255, 23, 20, 184),
+        title: const Text(
+          'Visitors List',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: const Color(0xFF800000),
+        iconTheme: const IconThemeData(
+          color: Colors.white, // Change the color of the back button to white
+        ),
         actions: [
           TextButton.icon(
             icon: const Icon(Icons.add, color: Colors.white),
@@ -90,7 +98,7 @@ class _VisitorsdataState extends State<Visitorsdata> {
               );
             },
             style: TextButton.styleFrom(
-              backgroundColor: const Color.fromARGB(255, 17, 157, 212),
+              backgroundColor: const Color(0xFF800000),
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(10),
               ),
@@ -120,103 +128,190 @@ class _VisitorsdataState extends State<Visitorsdata> {
           Expanded(
             child: isLoading
                 ? const Center(child: CircularProgressIndicator())
-                : DataTable2(
-                    columnSpacing: 0,
-                    horizontalMargin: 0,
-                    minWidth: 370,
-                    dataRowHeight: 50,
-                    headingRowHeight: 50,
-                    columns: const [
-                      DataColumn2(
-                        label: Center(
-                          child: Text(
-                            'Full Name',
-                            style: TextStyle(
+                : ListView.builder(
+                    itemCount: visitors.length,
+                    itemBuilder: (context, index) {
+                      final visitor = visitors[index];
+                      return Card(
+                        margin: const EdgeInsets.symmetric(
+                            vertical: 8.0, horizontal: 16.0),
+                        color: Colors.grey[300],
+                        elevation: 4,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: ListTile(
+                          contentPadding: const EdgeInsets.all(16.0),
+                          title: Text(
+                            visitor['full_name'] ?? 'N/A',
+                            style: const TextStyle(
+                              fontSize: 16,
                               fontWeight: FontWeight.bold,
-                              fontSize: 14,
                               color: Colors.black,
                             ),
                           ),
-                        ),
-                        size: ColumnSize.L,
-                      ),
-                      DataColumn(
-                        label: Center(
-                          child: Text(
-                            'Number',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 14,
-                              color: Colors.black,
-                            ),
-                          ),
-                        ),
-                      ),
-                      DataColumn(
-                        label: Center(
-                          child: Text(
-                            'Purpose',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 14,
-                              color: Colors.black,
-                            ),
-                          ),
-                        ),
-                      ),
-                      DataColumn(
-                        label: Center(
-                          child: Text(
-                            'Meeting',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 14,
-                              color: Colors.black,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                    rows: visitors.map<DataRow>((visitor) {
-                      return DataRow(
-                        cells: [
-                          DataCell(Center(
-                            child: Text(
-                              visitor['full_name'] ?? 'N/A',
-                              style: const TextStyle(
-                                fontSize: 13,
-                                fontWeight: FontWeight.w500,
-                                color: Colors.black,
+                          subtitle: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Mobile No: ${visitor['mobile_no'] ?? 'N/A'}',
+                                style: const TextStyle(
+                                    fontSize: 14, color: Colors.black),
                               ),
-                            ),
-                          )),
-                          DataCell(Center(
-                            child: Text(
-                              visitor['number'] ?? 'N/A',
-                              style: const TextStyle(
-                                  fontSize: 13, color: Colors.black),
-                            ),
-                          )),
-                          DataCell(Center(
-                            child: Text(
-                              visitor['purpose'] ?? 'N/A',
-                              style: const TextStyle(
-                                  fontSize: 13, color: Colors.black),
-                            ),
-                          )),
-                          DataCell(Center(
-                            child: Text(
-                              visitor['meeting_person'] ?? 'N/A',
-                              style: const TextStyle(
-                                  fontSize: 13, color: Colors.black),
-                            ),
-                          )),
-                        ],
+                              Text(
+                                'Location: ${visitor['location'] ?? 'N/A'}',
+                                style: const TextStyle(
+                                    fontSize: 14, color: Colors.black),
+                              ),
+                              Text(
+                                'Appointment: ${visitor['appointment'] ?? 'N/A'}',
+                                style: const TextStyle(
+                                    fontSize: 14, color: Colors.black),
+                              ),
+                              Text(
+                                'Email: ${visitor['email'] ?? 'N/A'}',
+                                style: const TextStyle(
+                                    fontSize: 14, color: Colors.black),
+                              ),
+                            ],
+                          ),
+                          onTap: () {
+                            // Navigate to the VisitorDetailScreen with visitor details
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) =>
+                                    VisitorDetailScreen(visitor: visitor),
+                              ),
+                            );
+                          },
+                        ),
                       );
-                    }).toList(),
+                    },
                   ),
           ),
         ],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        height: 55.0,
+        color: const Color(0xFF800000),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            IconButton(
+              icon: const Icon(Icons.home, color: Colors.white),
+              onPressed: () {
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (context) => const FrontPage()),
+                  (Route<dynamic> route) => false,
+                );
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.settings, color: Colors.white),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const SettingsScreen()),
+                );
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.person, color: Colors.white),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const ProfileScreen()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class VisitorDetailScreen extends StatelessWidget {
+  final dynamic visitor;
+
+  const VisitorDetailScreen({super.key, required this.visitor});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Visitor Details',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: const Color(0xFF800000),
+        iconTheme: const IconThemeData(
+          color: Colors.white, // Change the color of the back button to white
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          // Ensure the content is scrollable on smaller screens
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _buildDetailCard('Full Name', visitor['full_name']),
+              _buildDetailCard('Mobile No', visitor['mobile_no']),
+              _buildDetailCard('Location', visitor['location']),
+              _buildDetailCard('Appointment', visitor['appointment']),
+              _buildDetailCard('Email', visitor['email']),
+              _buildDetailCard('Company', visitor['company']),
+              _buildDetailCard('Visiting For', visitor['visiting_for']),
+              _buildDetailCard('Message', visitor['message']),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  // Helper widget for building detail cards
+  Widget _buildDetailCard(String title, String? value) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 12.0),
+      child: Card(
+        elevation: 2,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            children: [
+              Text(
+                '$title: ',
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              Expanded(
+                child: Text(
+                  value ?? 'N/A',
+                  style: const TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey,
+                  ),
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
